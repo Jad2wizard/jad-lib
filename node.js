c81ce55fbaf0cefd20f9de52f68495ed82cdc54a
Jad2Wizard <526654076@qq.com>
2020-03-05 11:05:23 +0800

    A: debounce && throttle

--- node.js
+++ node.js
@@ -1,8 +1,12 @@
+
 const num2Chinese = require('./lib/num2Chinese')
+const {rmdirSync, countFiles} = require('./lib/node')
 const {debounce, throttle} = require('./lib/utils')
 
 module.exports = {
     num2Chinese,
     debounce,
-    throttle
+    throttle,
+    rmdirSync,
+    countFiles
 }
