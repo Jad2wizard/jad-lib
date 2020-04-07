30236c1c54bdd19b39323dcf5bbffb71e9a85ced
Jad2Wizard <526654076@qq.com>
2020-04-07 17:30:35 +0800

    A: math library && declaration file of typescript

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module 'jadwizard-lib' {
+	export function num2Chinese(num: number | string): string
+	export module utils {
+		export function debounce<T extends (...args: any[]) => any>(fn: T): T
+		export function throttle<T extends (...args: any[]) => any>(fn: T): T
+	}
+	export module math {
+		export function sum(v: number[]): number
+		export function average(v: number[]): number
+		export function stdDev(v: number[]): number
+		export function cov(v1: number[], v2: number[]): number
+		export function corr(v1: number[], v2: number[]): number
+	}
+	export module is {
+		export function defined(o: any): boolean
+		export function string(o: any): boolean
+		export function fn(o: any): boolean
+		export function array(o: any): boolean
+		export function plainObject(o: any): boolean
+		export function object(o: any): boolean
+		export function emptyObj(o: any): boolean
+		export function number(o: any): boolean
+		export function integer(o: any): boolean
+		export function bool(o: any): boolean
+		export function emptyString(o: any): boolean
+		export function nonEmptyString(o: any): boolean
+		export function promise(o: any): boolean
+	}
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const num2Chinese = require('./lib/num2Chinese')
 const utils = require('./lib/utils')
 const is = require('./lib/is')
+const math = require('./lib/math')
 
 module.exports = {
-    num2Chinese,
-    utils,
-    is
+	num2Chinese,
+	utils,
+	is,
+	math,
 }
